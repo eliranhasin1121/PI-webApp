5da5a7c5a57df884ff30f055fd6dd88db3654a27
Eliran Hasin <ehasin@medallia.com>
2021-04-02 16:43:17 +0300

    WIP

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -2,21 +2,34 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { Card, Button } from 'antd';
-import { Icon } from '@ant-design/compatible';
+// import { Icon } from '@ant-design/compatible';
 import { DownCircleTwoTone,DownCircleOutlined } from '@ant-design/icons';
-import { MainWrapper,SectionA,IconWrapper, SectionB, LeftWrapper, PiWrapper, RightWrapper,TextWrapper,SectionC,SectionCTextWrapper, BoldText, RegularText, SectionD } from './Main.styled';
+import { MainWrapper,SectionA,IconWrapper, SectionB, LeftWrapper, PiWrapper, RightWrapper,TextWrapper,SectionC,SectionCTextWrapper, BoldText, RegularText, SectionD,SectionE,SectionETextWrapper,SectionF,MainText,SecondaryText, OurProductsStyled, ProductsContainer, ProductWrapper, LogoText,Icon,SectionG, ArrowWrapper, OtherProducts } from './Main.styled';
 import { changeLanguage, getActiveLanguage } from '../../config/translations/utils';
 import { Text, FlexDivCenter } from '../../components/Typography/Typography';
+import {PiNovel,PIEmerging,PiOpportunites, ArrowDown} from '../../common/icons/iconSVG';
 // import { loginAction } from '../../data/modules/auth/auth.actions';
+import opportImg from '../../assets/page1/opportunities.png';
+import emerging from '../../assets/page1/emerging.png';
+import novel from '../../assets/page1/novel.png';
 
-export default () => {
+export default function Main({}){
 
+
+  // const Product = (logo,text) => (
+  //   <ProductWrapper>
+  //    <Icon img={opportImg}/>
+  //     <LogoText>
+  //       {text}
+  //     </LogoText>
+  //   </ProductWrapper>
+  // )
 
   return (
     <MainWrapper>
       <SectionA>
         <IconWrapper>
-        <DownCircleOutlined className={'down'} /> 
+
       </IconWrapper>
       </SectionA>
       <SectionB>
@@ -46,6 +59,53 @@ Mr. Kaye started his career in 1992 trading listed options for Susquehana Invest
     </SectionCTextWrapper>
       </SectionC>
       <SectionD/>
+      <SectionE>
+        <SectionETextWrapper>
+            <BoldText>
+              Ahron Cohen
+            </BoldText>
+            <RegularText>
+            Mr. Cohen is the co-founder of PI Group, which was founded in 1999. Mr. Cohen is leading the business development of the group and represents the Fund with Institutional investors in Israel.  
+Mr. Cohen has over 23 years' extensive experience in financial markets, mainly in private capital investments, Institutional investments and M&A in the public sector.
+Prior to founding PI Group, Mr. Cohen managed the stock options desk at Mizrahi Tefahot Bank Ltd. Between 1997 to 1999 Mr. Cohen was a senior Vice President at Clal finance Ltd, where he oversaw the proprietary and derivatives trading desks.
+            </RegularText>
+        </SectionETextWrapper>
+      </SectionE>
+      <SectionF>
+        <OurProductsStyled>
+          <MainText>OUR PRODUCTS</MainText>
+          <SecondaryText>HEDGE FUNDS</SecondaryText>
+        </OurProductsStyled>
+        <ProductsContainer>
+        <ProductWrapper>
+     <Icon img={novel}/>
+      <LogoText>
+       PI NOVEL
+      </LogoText>
+    </ProductWrapper>
+    <ProductWrapper>
+     <Icon img={emerging}/>
+      <LogoText>
+      PI EMERGING MARKETS
+      </LogoText>
+    </ProductWrapper>
+    <ProductWrapper>
+     <Icon img={opportImg}/>
+      <LogoText>
+      PI OPPORTUNITIES
+      </LogoText>
+    </ProductWrapper>
+        </ProductsContainer>
+      </SectionF>
+      <SectionG>
+        <ArrowWrapper>
+        <ArrowDown/>
+        </ArrowWrapper>
+        <OtherProducts>
+          <MainText color={'#fffff'}>OUR PRODUCTS</MainText>
+          <SecondaryText opacity={0.7}>other</SecondaryText>
+        </OtherProducts>
+      </SectionG>
     </MainWrapper>
   );
 };
--- src/containers/Main/Main.styled.js
+++ src/containers/Main/Main.styled.js
@@ -3,6 +3,8 @@ import sectionABackground from '../../assets/page1/sectionA.jpg';
 import PiIcon from '../../assets/page1/pi.jpg';
 import sectionCImage from '../../assets/page1/sectionC.jpg';
 import timeLine from '../../assets/page1/timeline.png';
+import sectionEBackground from '../../assets/page1/sectionE.jpg';
+
 export const MainWrapper = styled('div')``;
 
 export const SectionA = styled.div`
@@ -14,17 +16,9 @@ background-size: cover;
 `
 
 export const IconWrapper = styled.div`
-    position: absolute;
-    top: 705px;
+    position: relative;
+    top: 693px;
     left: 50%;
-    .down{
-    font-size:40px;
-    border-radius:20px;
-    box-shadow:7px 6px 28px 1px rgba(0, 0, 0, 0.24);
-    cursor:pointer;
-    outline: none; 
-    transition: 0.2s all;
-    }
 `
 
 export const SectionB = styled.div`
@@ -87,11 +81,13 @@ font-size:28px;
 padding:30px 0px;
 text-align:center;
 font-weight:bold;
+color:black;
 `
 
 export const RegularText = styled.div`
 font-size:22px;
 padding:0px 25px;
+text-align:left;
 `
 
 export const SectionD = styled.div`
@@ -100,4 +96,100 @@ width:100%;
 background-image:url(${timeLine});
 background-size:cover;
 background-repeat:no-repeat;
+`
+
+export const SectionE = styled.div`
+background-image:url(${sectionEBackground});
+background-repeat:no-repeat;
+background-size:cover;
+height:750px;
+width:100%;
+display: flex;
+justify-content: flex-end;
+`
+
+export const SectionETextWrapper = styled.div`
+height:100%;
+width:20%;
+background-color:#ffffff;
+opacity:0.7;
+margin-right:70px;
+`
+
+export const SectionF = styled.div`
+height:725px;
+width:100%;
+`
+
+export const OurProductsStyled = styled.div`
+display:flex;
+flex-direction:column;
+justify-content:center;
+padding-top:50px;
+text-align:center;
+`
+
+export const MainText = styled.div`
+font-family: Helvetica;
+font-size: 25px;
+color:${({color}) => color ||'#000000' };
+letter-spacing: -1.41px;
+text-align: center;
+`
+
+export const SecondaryText = styled.div`
+font-family: Helvetica;
+font-size: 20px;
+letter-spacing: -1.12px;
+text-align: center;
+opacity:${({opacity}) => opacity || 1};
+`
+
+export const ProductsContainer = styled.div`
+width:60%;
+padding-top:170px;
+display:flex;
+justify-content:space-evenly;
+margin:auto;
+`
+export const ProductWrapper = styled.div`
+display:flex;
+flex-direction:column;
+align-items: center;
+`
+
+export const LogoText = styled.div`
+padding-top:12px;
+font-size:16px;
+font-family: Helvetica;
+letter-spacing: -1.41px;
+text-align: center;
+`
+
+export const Icon  = styled.div`
+background-image:url(${({img}) => img});
+background-size:cover;
+background-repeat:no-repeat;
+height: 120px;
+width: 120px;
+border-radius: 120px;
+`
+
+export const SectionG = styled.div`
+width:100%;
+height:166px;
+background: #919191;
+`
+
+export const ArrowWrapper = styled.div`
+    position: relative;
+    left: 47%;
+    top: -42px;
+`
+
+export const OtherProducts = styled.div`
+    position: relative;
+    bottom: 30px;
+    right: 13px;
+    color:#ffffff;
 `
